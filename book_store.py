559d590cb0fbf609a1a52460826eba80bbad730e
Thierno Balde <dev.balde17@gmail.com>
2020-12-30 10:22:07 -0500

    Taking a new approach to the problem

--- book_store.py
+++ book_store.py
@@ -1,21 +1,17 @@
+
 import requests
 from bs4 import BeautifulSoup
-page = requests.get('http://books.toscrape.com/catalogue/10-day-green-smoothie-cleanse-lose-up-to-15-pounds-in-10-days_581/index.html')
-soup = BeautifulSoup(page.content, 'html.parser')
 
+URL = "http://books.toscrape.com/catalogue/page-"
+HTML_EXT = ".html"
 
-#get the product information
-product_information = soup.find('table', {'class': 'table table-striped'}).get_text()
-print(product_information)
+for p in range(50): #there are 50 pages
+   pageNumber = p + 1
+   url = URL + str(pageNumber) + HTML_EXT
+   print("########## Page " + str(pageNumber) + " ##########")
+   page=requests.get(url)
+   soup = BeautifulSoup(page.content, 'html.parser')
 
-#get the product description
-product_description = soup.find("p", class_=None).get_text()
-print(product_description)
-
-
-
-# Trying a different way to get the image_url
-image_url = soup.find_all('img', limit=1)
-for image in image_url:
-   print(image['src'])
-
+   for link in soup.find_all("a"):
+      if link.get("title") is not None:
+         print("Book title: {}".format(link.get("title")))
