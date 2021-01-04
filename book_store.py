68f780f9e837c8f5af407ec5710545c75fd78565
Thierno Balde <dev.balde17@gmail.com>
2021-01-03 22:15:20 -0500

    new approach to the project

--- book_store.py
+++ book_store.py
@@ -1,43 +1,82 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import csv
 
-page = requests.get('http://books.toscrape.com/catalogue/10-day-green-smoothie-cleanse-lose-up-to-15-pounds-in-10-days_581/index.html')
-soup = BeautifulSoup(page.content, 'html.parser')
+baseurl = 'http://books.toscrape.com/'
 
-
-#building a dictionary
-product_page_url=[]
-universal_product_code=[]
-title=[]
-price_including_tax=[]
-price_excluding_tax=[]
-number_available=[]
-product_description=[]
-category=[]
-review-rating=[]
-image_url=[]
-
-#get the product information
-product_information = soup.find('table', {'class': 'table table-striped'}).get_text()
-print(product_information)
-
-#get the product description
-product_description = soup.find("p", class_=None).get_text()
-print(product_description)
+headers ={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
 
 
 
-# Trying a different way to get the image_url
-image_url = soup.find_all('img', limit=1)
-for image in image_url:
-   print(image['src'])
+product_url = []
 
-# trying to organize the data
+r = requests.get('http://books.toscrape.com/catalogue/category/books/health_47/index.html')
+soup = BeautifulSoup(r.content, 'lxml')
+product_list = soup.find_all('div', class_="image_container")
+for image_container in product_list:
+    for urls in image_container.find_all('a', href=True):
+        product_url.append(baseurl + urls['href'])
+    prod_url = 'http://books.toscrape.com/catalogue/'+urls['href']
+    product_page_url = prod_url.replace("../","")
+    
+        
 
-data = {'Product_page': product_page_url, 'upc': universal_ product_code, 'Title': title, 'Price_i_tax': price_including_tax,
-'Price_e_tax': price_excluding_tax, 'Number' : number_available, 'Product': product_description, 'Category': category, 'Rating': review_rating,
-'Image': image_url}
+        
+            
+        
 
-df=pd.DataFrame(data=data)
-print(df)
+
+for urls in product_url:
+    r = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(r.content, 'lxml')
+    
+    product_page_url = 'http://books.toscrape.com/catalogue/10-day-green-smoothie-cleanse-lose-up-to-15-pounds-in-10-days_581/index.html'
+
+    title = soup.find('li', class_='active').text.strip()
+    
+
+
+    product_description = soup.find("p", class_=None).text.strip()
+    print(product_description)
+
+   
+
+    for r in soup.find_all("p", class_="star-rating Five"):
+        for k, v in r.attrs.items():
+            star = v[1]
+    
+        
+    image_urls = soup.find_all('img', limit=1)
+    for image in image_urls:
+    
+        img_url = 'http://books.toscrape.com/'+image['src']
+        image_url = img_url.replace("../","")
+
+    
+
+    book = {
+        'product_page_url':product_page_url,
+        'title':title,
+        'product_description':product_description,
+        'star':star,
+        'image_url':image_url
+            }
+ 
+
+      
+   
+
+    
+
+
+
+    
+    
+        
+     
+
+
+
+
