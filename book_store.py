fe4394f41c4f20a06a3223a38a6f008e1296e84f
Thierno Balde <dev.balde17@gmail.com>
2021-01-08 10:32:17 -0500

    generated 4 metadata

--- book_store.py
+++ book_store.py
@@ -1,82 +1,66 @@
 import requests
-from bs4 import BeautifulSoup
-<<<<<<< HEAD
 import pandas as pd
-import csv
+from bs4 import BeautifulSoup as bs4
 
-baseurl = 'http://books.toscrape.com/'
-
-headers ={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
-
-
-
-product_url = []
-
-r = requests.get('http://books.toscrape.com/catalogue/category/books/health_47/index.html')
-soup = BeautifulSoup(r.content, 'lxml')
-product_list = soup.find_all('div', class_="image_container")
-for image_container in product_list:
-    for urls in image_container.find_all('a', href=True):
-        product_url.append(baseurl + urls['href'])
-    prod_url = 'http://books.toscrape.com/catalogue/'+urls['href']
-    product_page_url = prod_url.replace("../","")
-    
-        
-
-        
-            
-        
-
-
-for urls in product_url:
-    r = requests.get(url, headers=headers)
-
-    soup = BeautifulSoup(r.content, 'lxml')
-    
-    product_page_url = 'http://books.toscrape.com/catalogue/10-day-green-smoothie-cleanse-lose-up-to-15-pounds-in-10-days_581/index.html'
-
-    title = soup.find('li', class_='active').text.strip()
-    
-
-
-    product_description = soup.find("p", class_=None).text.strip()
-    print(product_description)
-
-   
-
-    for r in soup.find_all("p", class_="star-rating Five"):
-        for k, v in r.attrs.items():
-            star = v[1]
-    
-        
-    image_urls = soup.find_all('img', limit=1)
-    for image in image_urls:
-    
-        img_url = 'http://books.toscrape.com/'+image['src']
-        image_url = img_url.replace("../","")
-
-# use a dictionary to get the information for one book
-
-    diet_book = {
-        'product_page_url':product_page_url,
-        'title':title,
-        'product_description':product_description,
-        'star':star,
-        'image_url':image_url
-            }
-    df = pd.DataFrame(diet_book)
-
-    print(df)
- 
-
-      
-   
-
-    
+baseurl ='http://books.toscrape.com/'
+pages = []
+product_page_urls = []
+titles = []
+image_urls = []
+review_ratings = []
 
 
 
-    
-    
-        
-     
+pages_to_scrape = 50
+
+# getting the urls for all 50 pages
+for i in range(1, pages_to_scrape+1):
+    url = ('http://books.toscrape.com/catalogue/page-{}.html').format(i)
+    pages.append(url)
+
+#creating a soup object to a book information
+for item in pages:
+   page = requests.get(item)
+   soup = bs4(page.text, 'html.parser')
+   print(soup.prettify())
+#creating a titles list by getting all the titles for page1
+   for i in soup.findAll('h3'):
+      title = i.getText()
+      titles.append(title)
+
+
+   product_list = soup.find_all('div', class_="image_container")
+   for image_container in product_list:
+      for urls in image_container.find_all('a', href=True):
+         prod_url = 'http://books.toscrape.com/catalogue/' + urls['href']
+         product_page_url = prod_url.replace("../", "")
+         product_page_urls.append(product_page_url)
+
+
+
+   image_url = soup.find_all('div', class_="image_container")
+   for image in image_url:
+      tgs=image.find('img', class_="thumbnail")
+      img_url = 'http://books.toscrape.com/' +str(tgs['src'])
+      imgs_urls = img_url.replace("../", "")
+      image_urls.append(imgs_urls)
+
+   for r in soup.find_all("p", class_="star-rating"):
+      for k, v in r.attrs.items():
+         star = v[1]
+         review_ratings.append(star)
+
+
+dict = {'product_page_url': product_page_urls, 'title': titles,
+    'image_url': image_urls, 'review_rating': review_ratings,
+    }
+
+print(dict)
+
+
+df = pd.DataFrame(data=dict)
+df.index+=1
+print(df)
+df.to_excel("/Users/balde/Webscraping/output.xlsx")
+
+print(df)
